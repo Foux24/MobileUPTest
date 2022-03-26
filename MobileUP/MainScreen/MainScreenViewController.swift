b917e2ceb72abe34a9de65b0668ea60556ab031b
Vitalya <foux24@ya.ru>
2022-03-26 13:23:01 +0300

    Подготовка проекта к началу работы

---
+++ MobileUP/MainScreen/MainScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainScreenViewController.swift
+//  MobileUP
+//
+//  Created by Vitalii Sukhoroslov on 26.03.2022.
+//
+
+import UIKit
+
+// MARK: - MainScreenViewController
+final class MainScreenViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
